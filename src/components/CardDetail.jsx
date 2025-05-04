05acdbbcf577a228e4fc7bf5edf0949369025243
Danny McCarragher <dannymccarragher@gmail.com>
2025-05-04 16:37:32 -0700

    Add Styling for Card Detail

--- src/components/CardDetail.jsx
+++ src/components/CardDetail.jsx
@@ -1,17 +1,24 @@
+import { Grid, Button, Typography, Card } from '@mui/material';
+
 const CardDetail = ({ pokemon }) => {
 
   // prevent the component from rendering until the types data is avalaible
   if (!pokemon.types || pokemon.types.length === 0) return null;
 
     return (
-      <div className="card-detail">
+      <Card className="card-detail" sx={{
+        width: "50%",
+        marginTop: 5,
+        marginLeft: 45
+        
+      }}>
         <h2>{pokemon.name}</h2>
         <p>Height: {pokemon.height} cm</p>
         <p>Weight: {pokemon.weight} kg</p>
         {/* type is nested within types object
          Get first index */}
         <p>Type: {pokemon.types[0].type.name}</p>
-        </div>
+        </Card>
     );
   };
   export default CardDetail;
