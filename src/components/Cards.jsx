74b6cdc7ead5968253df6e5dd2f010f737b9d7a6
Danny McCarragher <dannymccarragher@gmail.com>
2025-05-04 15:07:47 -0700

    Changed to for loop to only render a list of 20

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import CardDetail from './CardDetail.jsx';
 
-const Cards = ({ pokemons }) => {
+const Cards = ({ pokemons, page}) => {
 
 
   const [selectedPokemon, setSelectedPokemon] = useState([]);
@@ -12,15 +12,27 @@ const Cards = ({ pokemons }) => {
     setSelectedPokemon(data);
   }
 
+  
+  const renderPokemon = [];
+
+  for(let i = page[0]; i < page[1] && i < pokemons.length; i++){
+    renderPokemon.push(
+      <div className="card" onClick={() => onPokemonClick(pokemons[i].url)}>
+        {pokemons[i].name}
+      </div>
+    )
+  }
+
+  // console.log(pokemons);
+  // console.log(page);
+
+
   // console.log(selectedPokemon)
 
 
   return (
     <div className="cards">
-      {pokemons.map((p, index) => (<div key={index} className="card" onClick={() => onPokemonClick(p.url)}>
-          {p.name}
-        </div>
-      ))}
+      {renderPokemon}
       {selectedPokemon && <CardDetail pokemon={selectedPokemon}></CardDetail>}
     </div>
   );
