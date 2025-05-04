2bca7656a1e3cbcf2175089245b9cead83c40f14
Danny McCarragher <dannymccarragher@gmail.com>
2025-05-04 14:14:27 -0700

    Implemented conditional rendering

--- src/components/CardDetail.jsx
+++ src/components/CardDetail.jsx
@@ -1,4 +1,8 @@
 const CardDetail = ({ pokemon }) => {
+
+  // prevent the component from rendering until the types data is avalaible
+  if (!pokemon.types || pokemon.types.length === 0) return null;
+
     return (
       <div className="card-detail">
         <h2>{pokemon.name}</h2>
